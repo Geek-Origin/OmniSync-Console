f7c73afd961b324935982c9145807d76a31a9608
BDGSL <kamg64670452@163.com>
2026-04-29 22:20:04 +0800

    初始化 Console 模块

---
+++ OmniSync-Console/src/main/java/org/example/OmniSync_Console/OmniSyncConsoleApplication.java
@@ -0,0 +1,13 @@
+package org.example.OmniSync_Console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniSyncConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniSyncConsoleApplication.class, args);
+	}
+
+}
